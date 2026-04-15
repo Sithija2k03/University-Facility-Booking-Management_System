75a0db3137bfba389108ab5e1fe528b069738974
Thinuri Chathma <thinurichathma2002@gmail.com>
2026-04-15 23:23:03 +0530

    Update App Router
    Add the new register route

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import ProtectedRoute from "../auth/ProtectedRoute";
 import Navbar from "../components/Navbar";
 import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
 import UserDashboard from "../pages/UserDashboard";
 import AdminDashboard from "../pages/AdminDashboard";
 import TechnicianDashboard from "../pages/TechnicianDashboard";
@@ -24,6 +25,7 @@ function AppRouter() {
       <Navbar />
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/unauthorized" element={<UnauthorizedPage />} />
 
         <Route
